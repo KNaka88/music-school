f5d1e7cebafc44eeade8304e07c355cdd93bb107
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-09-24 13:31:51 -0700

    add login functionallity on login.component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,10 +10,15 @@ import { UserService } from '../service/user/user.service';
 })
 export class LoginComponent implements OnInit {
 
+    email: string;
+    password: string;
     auth: any;
     state: string = '';
+    error: any;
+    verifyRequired: boolean = false;
+    isVerifyEmailSent: boolean = false;
 
-    constructor(private userService: UserService, private router: Router) {
+    constructor (private userService: UserService, private router: Router) {
         this.userService.getAuth().subscribe(auth => {
             if (auth) {
                 this.router.navigateByUrl('/dashboard');
@@ -22,5 +27,34 @@ export class LoginComponent implements OnInit {
     }
 
     ngOnInit() {
+    }
+
+    login() {
+      this.userService.login(this.email, this.password).then((user) => {
+
+        //check if user verified the email (boolean)
+        if (user.emailVerified) {
+            this.router.navigateByUrl('dashboard');
+
+        } else {
+          //false
+          this.verifyRequired = true;
+        }
+      })
+      .catch((error: any) => {
+        if (error) {
+          this.error = error;
+          alert(error.message);
+        }
+      });
+
+      this.password = '';
+    }
+
+    sendVerifyEmail(){
+      this.userService.getAuth().subscribe( (auth) => {
+        this.isVerifyEmailSent = true;
+        auth.sendEmailVerification();
+      });
     }
 }
