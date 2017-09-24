d1b1dd809b02a96613d16ce533573ca0460316e5
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-09-24 13:15:38 -0700

    add redirect functionality on login.component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../service/user/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    auth: any;
+    state: string = '';
 
-  ngOnInit() {
-  }
+    constructor(private userService: UserService, private router: Router) {
+        this.userService.getAuth().subscribe(auth => {
+            if (auth) {
+                this.router.navigateByUrl('/dashboard');
+            }
+        });
+    }
 
+    ngOnInit() {
+    }
 }
