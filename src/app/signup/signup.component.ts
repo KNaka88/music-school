cccef0d7b216ba2611f5a48ba11993117b6f0c5a
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-09-24 15:49:03 -0700

    add redirect functionality on signup.component

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -31,11 +31,13 @@ export class SignupComponent implements OnInit {
       public isEmailVerified: boolean;
       public emailValidation: RegExp = new RegExp( /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
-
-      constructor(
-        private userService: UserService,
-        private router: Router,
-      ) { }
+      constructor (private userService: UserService, private router: Router) {
+          this.userService.getAuth().subscribe(auth => {
+              if (auth && auth.emailVerified != null) {
+                  this.router.navigateByUrl('/dashboard');
+              }
+          });
+      }
 
       ngOnInit() {
       }
@@ -66,14 +68,14 @@ export class SignupComponent implements OnInit {
 
       createAccount() {
         this.userService.registerUser(this.email, this.password).then( (user) => {
-                let newUser = new User(user.uid, this.firstName, this.lastName, this.email, this.phoneNumber);
-                this.userService.createAccount(newUser).then(() => {
-                    this.userService.getAuth().subscribe( (auth) => {
-                        auth.sendEmailVerification().then( () => {
-                            this.router.navigate(['/verify-account']);
-                        });
+            let newUser = new User(user.uid, this.firstName, this.lastName, this.email, this.phoneNumber);
+            this.userService.createAccount(newUser).then(() => {
+                this.userService.getAuth().subscribe( (auth) => {
+                    auth.sendEmailVerification().then( () => {
+                        this.router.navigate(['/verify-account']);
                     });
-                })
-            })
-        }
+                });
+            });
+        });
     }
+}
