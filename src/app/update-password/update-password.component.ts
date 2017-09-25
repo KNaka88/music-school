b10d6c54ca10a67bd370e6ddfb6f55b92b5068c1
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-09-24 17:13:24 -0700

    comment out ngcheck

--- src/app/update-password/update-password.component.ts
+++ src/app/update-password/update-password.component.ts
@@ -23,25 +23,24 @@ export class UpdatePasswordComponent {
       private userService: UserService,
     ) { }
 
-
-    ngDoCheck(){
-      //if password length is more than 6, change color to green
-      if (this.newPassword.length >= 6) {
-        this.isPasswordLength = true;
-      } else {
-        this.isPasswordLength = false;
-      }
-
-      //if password and confirm password matches, change color to green
-      if (this.newPassword === this.confirmPassword && this.newPassword.length >= 6) {
-        this.isPasswordMatch = true;
-      } else {
-        this.isPasswordMatch = false;
-      }
-    }
+    //
+    // ngDoCheck(){
+    //   //if password length is more than 6, change color to green
+    //   if (this.newPassword.length >= 6) {
+    //     this.isPasswordLength = true;
+    //   } else {
+    //     this.isPasswordLength = false;
+    //   }
+    //
+    //   //if password and confirm password matches, change color to green
+    //   if (this.newPassword === this.confirmPassword && this.newPassword.length >= 6) {
+    //     this.isPasswordMatch = true;
+    //   } else {
+    //     this.isPasswordMatch = false;
+    //   }
+    // }
 
     confirm() {
-
       let promise = new Promise((resolve) => {
         let credential = this.userService.getCredentials(this.email, this.password);
         resolve(credential);
@@ -60,8 +59,8 @@ export class UpdatePasswordComponent {
 
     updatePassword(){
       //check if form is typed correct
-      let verified = (this.isPasswordMatch && this.isPasswordLength);
-      if(verified) {
+    //   let verified = (this.isPasswordMatch && this.isPasswordLength);
+    //   if(verified) {
         this.userService.updatePassword(this.newPassword).then( () => {
             //success
             //clear the form
@@ -73,5 +72,5 @@ export class UpdatePasswordComponent {
             console.log(error);
         };
       }
-    }
+    // }
 }
